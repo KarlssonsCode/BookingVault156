a1de73ce36cf1d73a98135c3215a428ecbbbf956
Douglas <doggek@hotmail.se>
2023-01-16 10:01:40 +0100

    Added VisualtStudio project

---
+++ BookingVault156/BookingVault156/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingVault156
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
